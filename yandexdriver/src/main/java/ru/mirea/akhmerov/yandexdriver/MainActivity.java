8cd3d3cc7f205fd9e37c1cb3ff961634097dabe4
ArslanFromTash <aakhmerov01@mail.ru>
2022-05-19 11:24:59 +0300

    Initial commit

---
+++ yandexdriver/src/main/java/ru/mirea/akhmerov/yandexdriver/MainActivity.java
@@ -0,0 +1,125 @@
+package ru.mirea.akhmerov.yandexdriver;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.RequestPoint;
+import com.yandex.mapkit.RequestPointType;
+import com.yandex.mapkit.directions.DirectionsFactory;
+import com.yandex.mapkit.directions.driving.DrivingOptions;
+import com.yandex.mapkit.directions.driving.DrivingRoute;
+import com.yandex.mapkit.directions.driving.DrivingRouter;
+import com.yandex.mapkit.directions.driving.DrivingSession;
+import com.yandex.mapkit.directions.driving.VehicleOptions;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DrivingSession.DrivingRouteListener{
+
+    private MapView mapView;
+    private static final int REQUEST_LOCATION = 11;
+    private final String MAPKIT_API_KEY = "cba89da5-c8fd-427b-ba9d-07e0939f2fa9";
+    private UserLocationLayer userLocationLayer;
+    private final Point ROUTE_START_LOCATION = new Point(55.670005, 37.479894);
+    private final Point ROUTE_END_LOCATION = new Point(55.794229, 37.700772);
+    private final Point SCREEN_CENTER = new Point(
+            (ROUTE_START_LOCATION.getLatitude() + ROUTE_END_LOCATION.getLatitude()) / 2,
+            (ROUTE_START_LOCATION.getLongitude() + ROUTE_END_LOCATION.getLongitude()) / 2);
+    private MapObjectCollection mapObjects;
+    private DrivingRouter drivingRouter;
+    private DrivingSession drivingSession;
+    private int[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        DirectionsFactory.initialize(this);
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
+
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.getMap().move(new CameraPosition(
+                SCREEN_CENTER, 5, 0, 0));
+        drivingRouter = DirectionsFactory.getInstance().createDrivingRouter();
+        mapObjects = mapView.getMap().getMapObjects().addCollection();
+
+        submitRequest();
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onDrivingRoutes(@NonNull List<DrivingRoute> list) {
+        int color;
+        for (int i = 0; i < list.size(); i++) {
+            // настроиваем цвета для каждого маршрута
+            color = colors[i];
+            // добавляем маршрут на карту
+            mapObjects.addPolyline(list.get(i).getGeometry()).setStrokeColor(color);
+        }
+    }
+    
+
+    @Override
+    public void onDrivingRoutesError(@NonNull Error error) {
+        String errorMessage = "unknown_error_message";
+        if (error instanceof RemoteError) {
+            errorMessage = "remote_error_message";
+        } else if (error instanceof NetworkError) {
+            errorMessage = "network_error_message";
+        }
+
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void submitRequest() {
+        DrivingOptions drivingOptions = new DrivingOptions();
+        VehicleOptions vehicleOptions = new VehicleOptions();
+        ArrayList<RequestPoint> requestPoints = new ArrayList<>();
+        requestPoints.add(new RequestPoint(
+                ROUTE_START_LOCATION,
+                RequestPointType.WAYPOINT,
+                null));
+        requestPoints.add(new RequestPoint(
+                ROUTE_END_LOCATION,
+                RequestPointType.WAYPOINT,
+                null));
+        drivingSession = drivingRouter.requestRoutes(requestPoints, drivingOptions, vehicleOptions, this);
+    }
+}
